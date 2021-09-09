8160a4e60d78e13f8b976ad86275d24159c42615
Gabriel <gabrieldenonifake@gmail.com>
2021-09-08 23:40:02 -0300

    Initial Commit

---
+++ iTunesPlayer/iTunesPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesPlayerApp.swift
+//  iTunesPlayer
+//
+//  Created by Gabriel on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct iTunesPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
