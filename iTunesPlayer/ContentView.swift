a44405476c86874e71c1527a6a93103b3eae4a6d
Gabriel <gabrieldenonifake@gmail.com>
2021-09-08 23:52:00 -0300

    Add initial list layout

--- iTunesPlayer/ContentView.swift
+++ iTunesPlayer/ContentView.swift
@@ -7,12 +7,51 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var artistNameInput = ""
+
+  var dummyData = ["Josh", "Bob", "Charlie", "Sam", "Tod"]
+
+  var body: some View {
+    HStack(spacing: 15) {
+      TextField("Song name...", text: $artistNameInput)
+        .textFieldStyle(RoundedBorderTextFieldStyle())
+    }.padding()
+
+    Spacer()
+
+    List(dummyData, id: \.self) { item in
+      ZStack {
+        Color.white.opacity(0.08)
+          .mask(RoundedRectangle(cornerRadius: 15))
+        HStack(spacing: 0) {
+          VStack(alignment: .leading) {
+            Text(item)
+              .font(.headline)
+              .lineLimit(2)
+            Text("Namerino")
+              .opacity(0.6)
+              .lineLimit(1)
+          }
+          Spacer()
+
+          .padding(.horizontal, 10)
+          ZStack {
+            RoundedRectangle(cornerRadius: 15)
+              .foregroundColor(Color.black.opacity(0.6))
+          }
+          .aspectRatio(1/1, contentMode: .fit)
+          .frame(width: 100)
+
+        }.padding()
+      }
     }
+  }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
